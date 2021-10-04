9c7bcda0b2a52c5aa0e1737edfabccbd17fede22
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2021-10-04 15:33:50 -0400

    Oct 3

---
+++ Assignment 5/src/ResizingArrayList.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class ResizingArrayList<E> implements Iterable<E> {
+   // the initial capacity of the underlying resizing array
+   private static final int INIT_CAPACITY = 10;
+
+   private E[] elements;   // the underlying array of generic elements
+   private int n;          // the number of elements in the list
+
+   /* Constructor: Initializes an empty list. */
+   public ResizingArrayList() {
+      elements = (E[]) new Object[INIT_CAPACITY];
+      n = 0;
+   }
+
+   // Returns the number of elements in the list.
+   public int size() { return n; }
+
+   // Is this list empty?
+   public boolean isEmpty() { return n == 0;}
+
+   // Resizes the underlying array holding the elements
+   private void resize(int capacity) {
+      assert capacity >= n;
+      elements = Arrays.copyOf(elements, capacity);
+   }
+
+   // Adds the item to the end of this list.
+   public void add(E element) { } // TODO: IMPLEMENT IN CLASS
+
+   // Adds the element at index.
+   public void add(int index, E element) { } // TODO: IMPLEMENT IN CLASS
+
+   // Replaces the element at index with the given element
+   public void set(int index, E element) { } // TODO: IMPLEMENT IN CLASS
+
+   // Returns the item at index without removing it
+   public E get(int index) { return elements[index]; } // TODO: IMPLEMENT IN CLASS
+
+   // Removes the element at position index
+   public void remove(int index) { } // TODO: IMPLEMENT IN CLASS
+
+   // Returns the index of the first occurrence of element or -1 if not found
+   public int indexOf(E element) { return -1; } // TODO: IMPLEMENT IN CLASS
+
+   @Override
+   // Returns an iterator that iterates over the items in this list
+   public Iterator<E> iterator() {return new ArrayIterator();}
+   // an iterator, doesn't implement remove() since it's optional
+   private class ArrayIterator implements Iterator<E> {
+      private int i = 0;
+      public boolean hasNext()  { return i < n;                               }
+      public void remove()      { throw new UnsupportedOperationException();  }
+
+      public E next() {
+         if (!hasNext()) throw new NoSuchElementException();
+         return elements[i++];
+      }
+   }
+
+   @Override
+   // returns a string representation of this list
+   public String toString() {
+      StringBuilder s = new StringBuilder("[ ");
+      if (n == 0) return "This list is empty";
+      for (E value : this)
+         s.append( value + " ");
+      s.append("]");
+      return s.toString();
+   }
+
+   public static void main(String[] args) {
+      ResizingArrayList<String> list = new ResizingArrayList<>();
+      System.out.println(list.isEmpty());
+      list.add("to"); list.add("be"); list.add("or"); list.add("not"); list.add("to"); list.add("be");
+      for (String s : list)
+         System.out.print(s + " ");
+      System.out.println();   // to be or not to be
+      System.out.println(list);
+      list.remove(3); // remove the element at index 3
+      System.out.println(list);
+      list.add(2, "awesome"); // add an element at index 2
+      System.out.println(list);
+      list.add(0, "Or");    // add an element at the beginning
+      list.add(3, "really"); // add an element at index 2
+      list.add("great"); // and an element at the end
+      System.out.println(list);
+      list.set(list.size() - 1, "good");  // set the last element
+      System.out.println(list);
+      list.set(7, "do");
+      System.out.println(list);
+   }
+}
---
+++ Assignment 5/src/TestResizingArray.java
@@ -0,0 +1,52 @@
+public class TestResizingArray {
+   public static void main(String[] args) {
+      ResizingArrayList<Integer> list = new ResizingArrayList<>();
+      System.out.println(list.size());  // 0
+      System.out.println(list.isEmpty()); // true
+
+      // UNCOMMENT THE FOLLOWING TESTS ONE-BY-ONE TO TEST YOUR METHODS
+//      /* TEST SET 1: testing add() */
+//      list.add(-12);  // [-12]
+//      for (int i = 1; i <= 8; i++)
+//         list.add(i);
+//      System.out.println(list);  // [ -12 1 2 3 4 5 6 7 8 ]
+//
+//
+//      /* TEST SET 2: testing remove() */
+//      list.remove(0);  // [ 1 2 3 4 5 6 7 8 ]
+//      list.remove(6);  // [ 1 2 3 4 5 6 8 ]
+//      System.out.println(list);
+//
+//      /* TEST SET 3: testing add(index, element) */
+//      list.add(2, -1); // add -1 at position 2: [ 1 2 -1 3 4 5 6 8 ]
+//      list.add(0,-3); // add -1 at position 0: [ -3 1 2 -1 3 4 5 6 8 ]
+//      System.out.println(list);
+//      //list.add(22, 9);  // throws an exception
+//      //list.add(-22, 9); // throws an exception
+//
+//      /* TEST SET 4: testing set() and get() */
+//      System.out.println(list.get(0)); // -3
+//      // System.out.println(list.get(-10)); // throws an exception
+//      // System.out.println(list.get(22)); // throws an exception
+//      System.out.println(list.get(7)); // 6
+//
+//      list.set(0, 1000); // [ 1000 1 2 -1 3 4 5 6 8 ]
+//      list.set(3, 2000); // [ 1000 1 2 2000 3 4 5 6 8 ]
+//      System.out.println(list);
+//      //list.set(list.size() + 10000, 8); // throws an exception
+//      //list.set(-1, 8); // throws an exception
+//      list.set(list.size() - 1, 99);
+//      System.out.println(list); // [ 1000 1 2 2000 3 4 5 6 99 ]
+//
+//      /* TEST SET 5: testing indexOf() */
+//      System.out.println(list.indexOf(99)); // 8
+//      list.set(0, 8); list.set(3, 8); // [ 8 1 2 8 3 4 5 6 99 ] - two values equal to 8
+//      System.out.println(list.indexOf(8)); // 0
+//      System.out.println(list.indexOf(5555)); // -1 (value not found)
+//
+//      /* TEST SET 6: testing iterator() for-each must work */
+//      for (Integer k : list)
+//         System.out.print(k + " ");
+//      //should print 8 1 2 8 3 4 5 6 99
+   }
+}
