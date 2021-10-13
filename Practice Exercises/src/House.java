f4f84d75d8073bd31f5d6ee86baa0cc00d4c9baf
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2021-10-13 16:16:39 -0400

    1

---
+++ Practice Exercises/src/House.java
@@ -0,0 +1,39 @@
+public class House {
+    public static void main(String[] args) {
+        int sum = 0;
+        int N = 10;
+
+        /*
+        for (int n = N; n > 0; n /= 2) {
+            System.out.println(sum);
+            for (int i = 0; i < n; i++) {
+                System.out.println(sum);
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        */
+
+        /*
+        for (int i = 1; i < N; i *= 2) {
+            System.out.println(sum);
+            for(int j = 0; j < i; j++) {
+                System.out.println(sum);
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        */
+
+        /*
+        for (int i = 1; i < N; i *= 2) {
+            System.out.println(sum);
+            for (int j = 0; j < N; j++) {
+                System.out.println(sum);
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        */
+    }
+}
