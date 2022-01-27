d514055ca264995b5c694fa4f470a1725c9336aa
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2022-01-26 21:13:31 -0500

    Yes

--- Practice Exercises/src/reverse.java
+++ Practice Exercises/src/reverse.java
@@ -1,23 +1,22 @@
-import java.util.*;
-
 public class reverse {
 	public static void main(String[] args) {
-		int a[] = {1,2,3,4,5};
-		
-		System.out.println(a.length - 1 - 0);
-		
-		//                2
-		for(int i=0; i<a.length / 2; i++) {
-		//  temp equals index 0
-			int temp = a[i];
-		
-		//	index 0 equals 
-			a[i] = a[a.length - 1 - i];
-			
-		//		
-			a[a.length - 1 - i] = temp;
+		String s = "abcdefg";
+		int k = 2;
+		reverseStr(s, k);
+	}
+
+	public static String reverseStr(String s, int k) {
+		for(int i=k; i<(s.length() / 2); i++) {
+			char c = s.charAt(i);
+			//  temp equals index 0
+			char temp = c;
+
+			//	index 0 equals
+			c = s.charAt(s.length() - 1 - i);
+
+			//
+			s.charAt(s.length() - 1 - i) = temp;
 		}
-		
-		System.out.println(Arrays.toString(a));
+		return s;
 	}
 }
